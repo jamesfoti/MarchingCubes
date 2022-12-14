b94618fd0827038d7a51c247e783292757bbfc59
James Foti <jameswilliamfoti@gmail.com>
2022-12-13 20:51:58 -0800

    refactor naming in OrbitalCameraScript.cs

--- Assets/Scripts/Camera/OrbitalCamera.cs
+++ Assets/Scripts/Camera/OrbitalCamera.cs
@@ -2,19 +2,19 @@ using UnityEngine;
 
 public class OrbitalCamera : MonoBehaviour
 {
-    public float MovementSpeed = 10f;
+    public float DragSpeed = 10f;
     public float RotationSpeed = 50f; 
 
-    private Vector3 _movementDirection = Vector3.zero;
+    private Vector3 _dragDirection = Vector3.zero;
     private Vector3 _rotationDirection = Vector3.zero;
 
 	private void Update()
     {
-        ReadInput();
-        ApplyInput();
+        ReadMovmentInput();
+        Move();
     }
 
-    private void ReadInput()
+    private void ReadMovmentInput()
 	{
         if (Input.GetMouseButton(1))
         {
@@ -24,18 +24,18 @@ public class OrbitalCamera : MonoBehaviour
 
         if (Input.GetMouseButton(2))
         {
-            _movementDirection.x = -Input.GetAxis("Mouse X");
-            _movementDirection.y = -Input.GetAxis("Mouse Y");
+            _dragDirection.x = -Input.GetAxis("Mouse X");
+            _dragDirection.y = -Input.GetAxis("Mouse Y");
         }
 
-        _movementDirection.z = Input.GetAxis("Mouse ScrollWheel");
+        _dragDirection.z = Input.GetAxis("Mouse ScrollWheel");
     }
 
-    private void ApplyInput()
+    private void Move()
 	{
-        if (_movementDirection.sqrMagnitude > 0)
+        if (_dragDirection.sqrMagnitude > 0)
         {
-            transform.Translate(_movementDirection * MovementSpeed * Time.deltaTime);
+            transform.Translate(_dragDirection * DragSpeed * Time.deltaTime);
         }
 
         if (_rotationDirection.sqrMagnitude > 0)
